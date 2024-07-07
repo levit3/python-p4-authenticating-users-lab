57adfbc1a0d95be120ca1098ae57e4636d7eae0e
Levi Mwendwa <lvmwendwa4@gmail.com>
2024-07-07 21:27:47 +0300

    completes lab

--- server/app.py
+++ server/app.py
@@ -47,11 +47,36 @@ class ShowArticle(Resource):
             return make_response(article_json, 200)
 
         return {'message': 'Maximum pageview limit reached'}, 401
+    
+class Login(Resource):
+    
+    def post(self):
+        user = User.query.filter_by(username = request.json.get('username')).first()
+        session['user_id'] = user.id
+        
+        return make_response(user.to_dict(), 200)
+        
+class Logout(Resource):
+    
+    def delete(self):
+        session['user_id'] = None
+        return {}, 204
+    
+class CheckSession(Resource):
+    
+    def get(self):
+        user_id = session.get('user_id')
+        if user_id:
+            return make_response(User.query.filter_by(id = user_id).first().to_dict(), 200)
+        return {}, 401 
+            
 
 api.add_resource(ClearSession, '/clear')
 api.add_resource(IndexArticle, '/articles')
 api.add_resource(ShowArticle, '/articles/<int:id>')
-
+api.add_resource(Login, '/login')
+api.add_resource(Logout, '/logout')
+api.add_resource(CheckSession, '/check_session')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
